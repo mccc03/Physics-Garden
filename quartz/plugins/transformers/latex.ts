dd9d8529efbf00819ad8a5690832de31ee27eb4a
iridhexx <sofia.cocciaretto@proton.me>
2025-03-16 02:40:39 +0100

    Quartz sync: Mar 16, 2025, 2:40 AM

--- quartz/plugins/transformers/latex.ts
+++ quartz/plugins/transformers/latex.ts
@@ -32,7 +32,7 @@ export const Latex: QuartzTransformerPlugin<Partial<Options>> = (opts) => {
     htmlPlugins() {
       switch (engine) {
         case "katex": {
-          return [[rehypeKatex, { output: "html", macros, ...(opts?.katexOptions ?? {}) }]]
+          return [[rehypeKatex, { macros, ...(opts?.katexOptions ?? {}) }]]
         }
         case "typst": {
           return [[rehypeTypst, opts?.typstOptions ?? {}]]
