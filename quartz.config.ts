1caa91152ce7517891107efa0d291f22dfc6807a
iridhexx <sofia.cocciaretto@proton.me>
2025-03-17 17:52:26 +0100

    Quartz sync: Mar 17, 2025, 5:52 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -6,6 +6,7 @@ import * as Plugin from "./quartz/plugins"
  *
  * See https://quartz.jzhao.xyz/configuration for more information.
  */
+// comment
 const config: QuartzConfig = {
   configuration: {
     pageTitle: "Physics Garden",
